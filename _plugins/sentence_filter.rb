070bb5db0a05b424b2270ee8caa2557f39ea3b3c
astrokoo7 <astrokbh7@example.com>
2023-07-28 19:34:25 +0900

    123123

--- _plugins/sentence_filter.rb
+++ _plugins/sentence_filter.rb
@@ -1,6 +1,8 @@
 module Jekyll
     module SentenceFilter
       def extract_first_sentence(input)
+        input = "Hello! This is a test. How are you doing?"
+
         # first_sentence = input.split(/[.!?]/).first
         # "#{first_sentence}." unless first_sentence.nil?
 
