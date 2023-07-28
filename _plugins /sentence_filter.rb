b5e0e481bd7d6b8efaa487cf052936b4e880f96b
astrokoo7 <astrokbh7@example.com>
2023-07-28 19:05:23 +0900

    1231

---
+++ _plugins /sentence_filter.rb
@@ -0,0 +1,10 @@
+module Jekyll
+    module SentenceFilter
+      def extract_first_sentence(input)
+        first_sentence = input.split(/[.!?]/).first
+        "#{first_sentence}." unless first_sentence.nil?
+      end
+    end
+  end
+  
+  Liquid::Template.register_filter(Jekyll::SentenceFilter)
