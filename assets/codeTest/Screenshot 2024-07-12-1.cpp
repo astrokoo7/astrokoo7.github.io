30e7eaaccf9779ff41e098f7014f5c9d435d9e0e
astrokoo7 <astrokbh7@example.com>
2024-07-23 06:54:40 +0900

    fwfwfw

---
+++ assets/codeTest/Screenshot 2024-07-12-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = -1;
+
+    sort(begin(A), end(A));
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
