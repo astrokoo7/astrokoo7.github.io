bbeca9245a60fdf380c92f02f67c2d57a8138447
astrokoo7 <astrokbh7@example.com>
2024-07-25 08:49:18 +0900

    fwfwfwfw

---
+++ assets/codeTest/Screenshot 2024-07-10-1.cpp
@@ -0,0 +1,11 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/152995
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// scores_rows는 2차원 배열 scores의 행 길이, scores_cols는 2차원 배열 scores의 열 길이입니다.
+int solution(int** scores, size_t scores_rows, size_t scores_cols) {
+    int answer = 0;
+    return answer;
+}
