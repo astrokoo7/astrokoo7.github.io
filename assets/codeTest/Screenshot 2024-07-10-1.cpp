bf1526ab0e49c169c74e75cabc5089bfc135e39f
astrokoo7 <astrokbh7@example.com>
2024-07-27 07:23:08 +0900

    fwfwfwf

--- assets/codeTest/Screenshot 2024-07-10-1.cpp
+++ assets/codeTest/Screenshot 2024-07-10-1.cpp
@@ -6,6 +6,18 @@
 using namespace std;
 
 int solution(vector<vector<int>> scores) {
-    int answer = 0;
+    int answer = 1;
+    int n = scores.size();
+    auto x = scores[0][0] + scores[0][1];
+    for (int i=1; i<n; i++) {
+        auto y = scores[i][0] + scores[i][1];
+        if (y > x) {
+            answer++;
+            if (scores[i][0] > scores[0][0] && 
+                scores[i][1] > scores[0][1]) {
+                return -1;
+            }
+        }
+    }
     return answer;
 }
