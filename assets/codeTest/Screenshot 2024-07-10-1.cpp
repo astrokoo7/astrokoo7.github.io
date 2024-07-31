4fe5f2be80ea2f1b526355172f250bd412bfcaeb
astrokoo7 <astrokbh7@example.com>
2024-08-01 08:02:24 +0900

    fwfwfwfw

--- assets/codeTest/Screenshot 2024-07-10-1.cpp
+++ assets/codeTest/Screenshot 2024-07-10-1.cpp
@@ -1,19 +1,33 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/152995
 
+#include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+bool cmp(vector<int> a, vector<int> b)
+{
+    return a[0] > b[0];
+}
+
 int solution(vector<vector<int>> scores) {
     int answer = 1;
     int n = scores.size();
+    int prevN = -1;
     auto x = scores[0][0] + scores[0][1];
-    for (int i=1; i<n; i++) {
+
+    sort(scores.begin()+1, scores.end(), cmp);
+
+    for (int i = 1; i < n; i++) {
         auto y = scores[i][0] + scores[i][1];
-        if (y > x) {
+
+        if (y > x && scores[i][1] >= prevN) {
+            prevN = scores[i][1];
+
             answer++;
-            if (scores[i][0] > scores[0][0] && 
+            if (scores[i][0] > scores[0][0] &&
                 scores[i][1] > scores[0][1]) {
                 return -1;
             }
@@ -22,3 +36,14 @@ int solution(vector<vector<int>> scores) {
     return answer;
 }
 
+
+int main()
+{
+    //완호보다 점수가 높은 사람중에 인센티브가 -1 인 사람이 있을 수 있습니다.
+    //[[4,1],[2,4],[3,5]]
+    //이 경우 완호 다음 직원은 완호보다 총 점은 높지만 마지막 직원 보다 점수가 둘다 낮기 때문에 인센티브를 받을 수 없고, 완호는 2등이 됩니다.
+
+    auto a = solution({ { 4, 1 }, { 2, 4 }, { 3, 5 } });
+
+    std::cout << "Hello World!\n";
+}
